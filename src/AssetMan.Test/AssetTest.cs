21b53b6bbb75595d6f39f3c7fc094b24072657b4
Kim Birkelund <kim@birkelund.me>
2019-10-11 09:12:38 +0200

    Overwrite older files. Fixes #2.

--- src/AssetMan.Test/AssetTest.cs
+++ src/AssetMan.Test/AssetTest.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using SkiaSharp;
+using Xunit;
 
 namespace AssetMan.Test
 {
@@ -7,8 +11,71 @@ namespace AssetMan.Test
         [Fact]
         public void Export()
         {
-            using (var asset = new Asset("../../../Images/logo@xxxhdpi.png"))
-                asset.Export("Images", 100, 100);
+            var inputPath = CreateInputFile();
+            var outputPath = GetTempPath();
+
+            using (var asset = new Asset(inputPath))
+                asset.Export(outputPath, 100, 100);
+
+            File.Exists(outputPath)
+                .Should()
+                .BeTrue();
+        }
+
+        [Fact]
+        public void Export_ShouldReplaceOlderFiles()
+        {
+            var inputPath = CreateInputFile();
+            var outputPath = GetTempPath();
+
+            using (var asset = new Asset(inputPath))
+                asset.Export(outputPath, 100, 100);
+
+            File.Exists(outputPath)
+                .Should()
+                .BeTrue();
+
+            var outputPathLastWriteTime = File.GetLastWriteTime(outputPath);
+
+
+            using (var asset = new Asset(inputPath))
+                asset.Export(outputPath, 100, 100);
+
+            File.GetLastWriteTime(outputPath)
+                .Should()
+                .Be(outputPathLastWriteTime);
+
+
+            File.SetLastWriteTime(inputPath,
+                                  File.GetLastWriteTime(outputPath)
+                                      .AddSeconds(1));
+
+            using (var asset = new Asset(inputPath))
+                asset.Export(outputPath, 100, 100);
+
+            File.GetLastWriteTime(outputPath)
+                .Should()
+                .BeAfter(outputPathLastWriteTime);
+        }
+
+        private static string CreateInputFile()
+        {
+            var inputPath = GetTempPath();
+
+            using (var stream = File.OpenWrite(inputPath))
+            {
+                SKBitmap.FromImage(SKImage.Create(new SKImageInfo(100, 100)))
+                        .PeekPixels()
+                        .Encode(SKPngEncoderOptions.Default)
+                        .SaveTo(stream);
+            }
+
+            return inputPath;
+        }
+
+        private static string GetTempPath()
+        {
+            return Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid():N}");
         }
     }
 }
